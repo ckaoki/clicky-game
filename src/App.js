6451d698f87c02914a6099557f030838089d2ec7
ckaoki <cullan_aoki@msn.com>
2019-07-30 19:40:18 -0700

    able to display character cards but not randmonly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Wrapper from './components/Wrapper';
+import Navbar from './components/navbar';
+import CharacterCard from './components/CharacterCard';
+import characters from './characters.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    characters
+   };
+
+  getCharacter = () => {return this.state.characters[0]};
+  
+  render(){
+    {console.log(this.state.characters[0].image)};
+    return (
+      <Wrapper>
+        {/* <Navbar /> */}
+        <Wrapper></Wrapper>
+        <Wrapper></Wrapper>
+        <Wrapper> 
+            {this.state.characters.map(character => (
+            <CharacterCard id={character.id} key={character.id} name={character.name} image={character.image}/>
+            ))}
+        </Wrapper>
+      </Wrapper>
+    )
+  }
 }
 
 export default App;
